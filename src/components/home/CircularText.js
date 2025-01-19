e1c7d588476798fb6b1267d28151b78bbb9ae411
Brendon Vicente <bredstone13@gmail.com>
2025-01-19 17:08:03 -0300

    fixing some bugs

--- src/components/home/CircularText.js
+++ src/components/home/CircularText.js
@@ -1,6 +1,6 @@
 export default function CircularText() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" className="circular-text">
+        <svg xmlns="http://www.w3.org/2000/svg" id="circular-text" viewBox="0 0 330 330" className="circular-text">
             <defs>
                 <path id="textcircle" d="M150,335a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z" transform="rotate(12,250,250)" />
             </defs>
