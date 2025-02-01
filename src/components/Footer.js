a3c5d82b2575223533873737010a16e935636127
Brendon Vicente <bredstone13@gmail.com>
2025-02-01 18:50:37 -0300

    finalizing ades mobile page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,29 +47,33 @@ export default function Footer() {
 }
 
 export function ProjectFooter({ nextProjectName, nextProjectSrc}) {
+    const windowBreakpoint = useContext(WindowSizeContext);
+    
     return (
-        <Container id="contact" className="mb-4 w-100 bg-primary rounded-5 p-5 text-white">
-            <Row className="py-4">
-                <Col>
-                    <h3 className="m-0 display-font">like what you see?</h3>
-                    <p className="subtitle mt-3">
-                        <AppleEmoji name="open-mailbox-with-raised-flag" className="me-3" />
-                        reach me at <a className="text-white" href="mailto:mannogiu@gmail.com">mannogiu@gmail.com</a>
-                    </p>
-                    <p className="subtitle mt-3">
-                        or at my <a className="text-white display-font subtitle" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/giulia-manno-88681a144">linkedin</a>
-                    </p>
-                </Col>
-
-                {/* <Col>
-                    <div className="w-100 h-100 d-flex flex-column align-items-end">
-                        <h3 className="m-0 display-font">next project</h3>
+        <div className="px-sm-0 px-4">
+            <Container id="contact" className="mb-4 bg-primary rounded-5 p-md-5 p-4 text-white">
+                <Row className="py-4">
+                    <Col>
+                        <h3 className="m-0 footer-title display-font">like what you see?</h3>
                         <p className="subtitle mt-3">
-                            <a href={nextProjectSrc} className="contact-link highlightable-inverted">{nextProjectName}<span className="arrow"> &#8594;</span></a>
+                            <AppleEmoji name="open-mailbox-with-raised-flag" className="me-3" width={windowBreakpoint === 'md' ? '3rem' : '1.5rem'} />
+                            reach me at <a className="text-white" href="mailto:mannogiu@gmail.com">mannogiu@gmail.com</a>
                         </p>
-                    </div>
-                </Col> */}
-            </Row>
-        </Container>
+                        <p className="subtitle mt-3">
+                            or at my <a className="text-white display-font subtitle" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/giulia-manno-88681a144">linkedin</a>
+                        </p>
+                    </Col>
+
+                    {/* <Col>
+                        <div className="w-100 h-100 d-flex flex-column align-items-end">
+                            <h3 className="m-0 display-font">next project</h3>
+                            <p className="subtitle mt-3">
+                                <a href={nextProjectSrc} className="contact-link highlightable-inverted">{nextProjectName}<span className="arrow"> &#8594;</span></a>
+                            </p>
+                        </div>
+                    </Col> */}
+                </Row>
+            </Container>
+        </div>
     );
 }
--- src/pages/AdesPage.js
+++ src/pages/AdesPage.js
@@ -5,7 +5,7 @@ import useDetectScroll from '@smakss/react-scroll-direction';
 
 import ParallaxImage from "components/project/ParallaxImage";
 import { imageList } from "components/project/ades/ImageList";
-import { ProjectFooter } from "components/Footer";
+import Footer from "components/Footer";
 
 export default function AdesPage({ setActiveSection, setImageIndex }) {
     const { ref: overviewRef, inView: overviewInView } = useInView({ rootMargin: "-20% 0px -30% 0px" });
@@ -37,7 +37,7 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
 
     return (
         <Container>
-            <Stack gap={5}>
+            <Stack className="gap-4 gap-xl-5">
                 <div ref={overviewRef} className="d-flex flex-column gap-4 gap-xl-5 px-4 px-sm-0">
                     {/* title */}
                     <Row id="overview">
@@ -112,7 +112,8 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
 
                 {/* challenges */}
                 <Row ref={challengesRef} id="challenges" className="gap-4">
-                    <Col xs={3}>
+                    <div className="gap-4 row px-4 px-sm-0">
+                    <Col md={3}>
                         <div className="w-100 h-100 d-flex align-items-center">
                             <h3 className="display-font">discovering the challenges</h3>
                         </div>
@@ -120,30 +121,31 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
 
                     <Col>
                         <div className="w-100 h-100 d-flex align-items-center">
-                            <div className="d-flex flex-column gap-3">
-                                <p>My client had extensive knowledge about most competitors' internal and technological functioning for their products and a firm belief that what they had come up with was unique and better for the end user. Even so, they lacked information about the user experience on other platforms, the users' pains and needs, and <b>how they could do better.</b></p>
-                                <p>This led us to conduct a <b>user survey</b> focusing on people who had already utilized some form of digital signature. Our survey ended up gathering over <b>200 responses</b>, making it easier to map out which signing platforms were the most utilized and what their users' challenges and motivations were.</p>
+                            <div className="d-flex flex-column gap-4 gap-md-3">
+                                <p>My client had extensive knowledge about most competitors' internal and technological functioning for their products and a firm belief that what they had come up with was unique and better for the end user.<br className="d-block d-md-none" /><br className="d-block d-md-none" /> Even so, they lacked information about the user experience on other platforms, the users' pains and needs, and <b>how they could do better.</b></p>
+                                <p>This led us to conduct a <b>user survey</b> focusing on people who had already utilized some form of digital signature.<br className="d-block d-md-none" /><br className="d-block d-md-none" /> Our survey ended up gathering over <b>200 responses</b>, making it easier to map out which signing platforms were the most utilized and what their users' challenges and motivations were.</p>
                             </div>
                         </div>
                     </Col>
+                    </div>
                 </Row>
 
-                <Row className="gap-4">
-                    <Col className="bg-secondary rounded-5 p-4">
+                <Row className="gap-3 gap-lg-4 px-4 px-sm-0">
+                    <Col lg xs={12} className="bg-secondary rounded-5 p-3 p-sm-4">
                         <div className="h-100 d-flex flex-column gap-2 mx-2 my-0">
                             <h5 className="display-text">User says:</h5>
                             <p>“I don't like going through an extensive and complicated process every single time I try to sign something.”</p>
                         </div>
                     </Col>
 
-                    <Col className="bg-secondary rounded-5 p-4">
+                    <Col lg xs={12} className="bg-secondary rounded-5 p-3 p-sm-4">
                         <div className="h-100 d-flex flex-column gap-2 mx-2 my-0">
                             <h5 className="display-text">User says:</h5>
                             <p>“The steps for authenticating and setting up my document for signing are too bureaucratic and/or take too long.”</p>
                         </div>
                     </Col>
 
-                    <Col className="bg-secondary rounded-5 p-4">
+                    <Col lg xs={12} className="bg-secondary rounded-5 p-3 p-sm-4">
                         <div className="h-100 d-flex flex-column gap-2 mx-2 my-0">
                             <h5 className="display-text">User says:</h5>
                             <p>“There are too many unecessary functionalities and it's hard to understand when you're a first-time user.”</p>
@@ -151,76 +153,110 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
                     </Col>
                 </Row>
 
-                <Row className="gap-5">
-                    <Col>
+                <Row className="gap-4 gap-lg-5">
+                    <Col className="px-4 px-sm-0">
                         <div className="d-flex flex-column h-100 justify-content-center gap-3">
-                            <p>My client had extensive knowledge about most competitors' internal and technological functioning for their products and a firm belief that what they had come up with was unique and better for the end user. Even so, they lacked information about the user experience on other platforms, the users' pains and needs, and <b>how they could do better.</b></p>
-                            <p>This led us to conduct a <b>user survey</b> focusing on people who had already utilized some form of digital signature. Our survey ended up gathering over <b>200 responses</b>, making it easier to map out which signing platforms were the most utilized and what their users' challenges and motivations were.</p>
-                            <p>This presented our opportunity:<br />to create a <b>public, free platform that is easy to use, even with high-level functionalities.</b></p>
+                            <p>Through the survey responses, considering the context of the product (the lab is located in Santa Catarina, Brazil), we analyzed our biggest competitors.</p>
+                            <p className="d-none d-lg-block">As we studied each platform's user interface and user experience, we concluded that <b>most complete, high-level systems belonged to big corporations focused on B2B clients.</b> While the Brazilian government made significant investments in its public signing platforms, most still lacked some important functionalities for my client and their user base, such as the capacity to co-sign documents with other parties.</p>
+                            <p className="d-none d-lg-block">This presented our opportunity:<br />to create a <b>public, free platform that is easy to use, even with high-level functionalities.</b></p>
                         </div>
                     </Col>
 
-                    <Col>
+                    <Col className="px-4 px-sm-0" lg>
                         <ParallaxImage parallaxSpeed={0} src={imageList[2].src} onClick={() => setImageIndex(2)} className="challenges-image shadow-none" />
+                    </Col>
+
+                    <Col className="px-4 px-sm-0 d-block d-lg-none">
+                        <div className="d-flex flex-column h-100 justify-content-center gap-3">
+                            <p>As we studied each platform's user interface and user experience, we concluded that <b>most complete, high-level systems belonged to big corporations focused on B2B clients.</b> While the Brazilian government made significant investments in its public signing platforms, most still lacked some important functionalities for my client and their user base, such as the capacity to co-sign documents with other parties.</p>
+                            <p>This presented our opportunity:<br />to create a <b>public, free platform that is easy to use, even with high-level functionalities.</b></p>
+                        </div>
                     </Col>
                 </Row>
 
                 {/* wireframes */}
-                <Row ref={wireframesRef} id="wireframes" className="py-4 gap-4">
-                    <Col xs={3}>
+                <Row ref={wireframesRef} id="wireframes" className="py-0 py-lg-4 gap-4">
+                    <Col className="px-4 px-sm-0" lg={3}>
                         <div className="w-100 h-100 d-flex align-items-center">
                             <h3 className="display-font">wireframes</h3>
                         </div>
                     </Col>
 
-                    <Col>
+                    <Col className="px-4 px-sm-0">
                         <div className="w-100 h-100 d-flex align-items-center">
-                            <p>The data from the discovery stage was essential for establishing the first iteration of prototypes and user flows, in a low-fidelity manner.  These wireframes described the flows for signing a document digitally, and were tested for possible usability issues, and to validate the suppositions that were made so far.</p>
+                            <p>The data from the discovery stage was essential for establishing the first iteration of prototypes and user flows, in a low-fidelity manner. <span className="d-none d-md-inline">These wireframes described the flows for signing a document digitally, and were tested for possible usability issues, and to validate the suppositions that were made so far.</span></p>
                         </div>
                     </Col>
                 </Row>
 
-                <Row>
+                <Row className="px-4 px-sm-0">
                     <ParallaxImage parallaxSpeed={0} src={imageList[3].src} onClick={() => setImageIndex(3)} className="viewport-panel teste bg-secondary" />
+                </Row>
+
+                <Row className="d-block d-md-none">
+                    <Col className="px-4 px-sm-0">
+                        <p>The main focus here was to represent the steps of digital signature from the point of view of someone uploading a document, divided in three flows: </p>
+                        <ul className="mt-3 mt-sm-4">
+                            <li>when only the user needs to sign it;</li>
+                            <li>when the user and other people need to sign it;</li>
+                            <li>when only other people have to sign it.</li>
+                        </ul>
+                    </Col>
                 </Row>
 
                 {/* tests and validation */}
                 <Row ref={testsRef} id="tests" className="gap-4">
-                    <Col>
+                    <Col className="px-4 px-sm-0 flex-basis-0">
                         <div className="w-100 h-100 d-flex flex-column gap-4 justify-content-center">
                             <h3 className="display-font">tests and validation</h3>
-                            <p>The wireframes underwent validation through a usability test, executed by a focal group; there were nine participants, divided in two groups of four and five people, each with a smartphone device containing the prototypes.</p>
-                            <p>This test was essential to validate the general navigation of the interface, as well as the disposition of elements and functionalities and their ease-of-use. The participants were encouraged to use the think-out-loud method to reach conclusions among the group, and they received paper and pens to take notes of their perceptions.</p>
-                            <p>After both groups finished their flows, the table discussed their thoughts and conclusions on what parts they liked and disliked the most. This input was essential in forming the final solution, as it helped lapidate the best course of action in terms of interaction and navigation.</p>
+                            <p>The wireframes underwent validation through a usability test, executed by a focal group; the participants were encouraged to use the <b>think-out-loud</b> method to reach conclusions among the group, and they received paper and pens to take notes of their perceptions.</p>
+                            <p className="d-none d-lg-block">Because the platform offered three distinct routes with different settings and functionalities, my main concern was offering clarity in terms of which one would be selected by the user. After the test, many feedbacks found this to be a redundant stage, which added one more step to the flow.</p>
+                            <p className="d-none d-lg-block">This led me to rethink the navigation structure, adding a dropdown component instead of the radio buttons, making the process more streamlined.</p>
+                            <p className="d-none d-lg-block">Another big input was regarding the dashboard, that showed a preview of each uploaded document. Users found this confusing and thought that it took too much space, and I reworked the screen to a list and grid formatting.</p>
                         </div>
                     </Col>
 
-                    <Col>
-                        <Row>
-                            <ParallaxImage src={imageList[4].src} onClick={() => setImageIndex(4)} className="horizontal-rectangle mb-4" />
+                    <Col lg className="d-md-flex flex-column">
+                        <Row className="flex-fill px-4 px-sm-0">
+                            <ParallaxImage parallaxSpeed={-4} src={imageList[4].src} onClick={() => setImageIndex(4)} className="horizontal-rectangle mb-3 mb-lg-4" />
                         </Row>
 
-                        <Row>
-                            <ParallaxImage src={imageList[5].src} onClick={() => setImageIndex(5)} className="horizontal-rectangle" />
+                        <Row className="flex-fill px-4 px-sm-0">
+                            <ParallaxImage parallaxSpeed={-4} src={imageList[5].src} onClick={() => setImageIndex(5)} className="horizontal-rectangle" />
                         </Row>
+                    </Col>
+
+                    <Col className="d-block d-lg-none px-4 px-sm-0">
+                        <p className="mb-4">Because the platform offered three distinct routes with different settings and functionalities, my main concern was offering clarity in terms of which one would be selected by the user. After the test, many feedbacks found this to be a redundant stage, which added one more step to the flow.</p>
+                        <p>This led me to rethink the navigation structure, adding a dropdown component instead of the radio buttons, making the process more streamlined.</p>
                     </Col>
                 </Row>
 
-                <Row className="gap-4">
-                    <ParallaxImage parallaxSpeed={-5} src={imageList[6].src} onClick={() => setImageIndex(6)} className="square" />
+                <Row className="d-lg-flex gap-4">
+                    <Col className="px-4 px-sm-0">
+                        <ParallaxImage parallaxSpeed={0} src={imageList[6].src} onClick={() => setImageIndex(6)} className="square" />
+                    </Col>
+                    
+                    <Col lg className="px-4 px-sm-0">
+                        <ParallaxImage parallaxSpeed={0} src={imageList[7].src} onClick={() => setImageIndex(7)} className="square" />
+                    </Col>
+                </Row>
 
-                    <ParallaxImage parallaxSpeed={-5} src={imageList[7].src} onClick={() => setImageIndex(7)} className="square" />
+                <Row className="d-block d-lg-none">
+                    <Col className="px-4 px-sm-0">
+                        <p>Another big input was regarding the dashboard, that showed a preview of each uploaded document. Users found this confusing and thought that it took too much space, and I reworked the screen to a list and grid formatting.</p>
+                    </Col>
                 </Row>
                 
                 {/* results */}
-                <Row ref={resultsRef} id="results" className="py-4 gap-4">
-                    <Col xs={3}>
+                <Row ref={resultsRef} id="results" className="py-0 py-lg-4 gap-4">
+                    <Col className="px-4 px-sm-0" lg={3}>
                         <div className="w-100 h-100 d-flex align-items-center">
                             <h3 className="display-font">results</h3>
                         </div>
                     </Col>
 
-                    <Col>
+                    <Col className="px-4 px-sm-0">
                         <div className="w-100 h-100 d-flex align-items-center">
                             <p>The full scope of the prototype consists of three navigable "routes" that encompass the experience of signing a document, preparing and signing a document and only preparing a document for third-party signature. The interactive prototype served not only as a deliverable for the client, but as documentation for their developers, since the product is set to be launched publicly in the next few years.</p>
                         </div>
@@ -228,10 +264,10 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
                 </Row>
 
                 <Row>
-                    <Col className="p-0">
+                    <Col className="px-4 px-sm-0">
                         <iframe
                             title="ADES - Figma project"
-                            className="viewport-panel rounded-5 shadow-sm w-100"
+                            className="figma-embed rounded-5 shadow-sm w-100"
                             src="https://embed.figma.com/proto/C4GEKRoJcTj55scYMjeSuP/TELAS-MOBILE?node-id=1252-8712&p=f&scaling=scale-down&content-scaling=fixed&page-id=1147%3A19431&starting-point-node-id=1252%3A8712&show-proto-sidebar=0&embed-host=share"
                             allowFullScreen>
                         </iframe>
@@ -240,7 +276,7 @@ export default function AdesPage({ setActiveSection, setImageIndex }) {
 
                 {/* footer */}
                 <Row ref={contactRef}>
-                    <ProjectFooter nextProjectName="FINDI - Digital Entertainment App" nextProjectSrc="#contact" />
+                    <Footer />
                 </Row>
             </Stack>
         </Container>
